52a361c4fbddb32c28ef09b4e1475eb88ba9e2b5
Jackai <jackpeng2012@126.com>
2024-02-05 04:04:51 -0500

    post game info added

--- backend/services/league_rank.py
+++ backend/services/league_rank.py
@@ -12,13 +12,15 @@ class Rank:
             url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}"
             headers = {"X-Riot-Token": self.RIOT_API_KEY}
             response = requests.get(url, headers=headers)
-
+            print(headers)
             if response.status_code == 200:
                 summoner_info = response.json()
+                summoner_puuid= summoner_info['puuid']
+                summoner_level = summoner_info['summonerLevel']
                 summoner_id = summoner_info['id']
                 if not summoner_id:
                     return "Summoner not found."
-            else:
+            else: 
                 return f"Error fetching summoner info: HTTP {response.status_code}"
 
             url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}"
@@ -30,11 +32,13 @@ class Rank:
                     rank_info = {
                         'tier': data[0].get('tier', 'N/A'),  
                         'rank': data[0].get('rank', 'N/A'), 
-                        'leaguePoints': data[0].get('leaguePoints', 0)
+                        'leaguePoints': data[0].get('leaguePoints', 0),
+                        'puuid':summoner_puuid,
+                        'level':summoner_level
                     }
-                    return rank_info
+                    return {'data' : rank_info}
                 else:
-                    return "No rank data found for the summoner."
+                    return {'msg':"No rank data found for the summoner."}
             else:
                 return f"Error fetching rank info: HTTP {response.status_code}"
 
