d4518e66ad81ecb3e4499f7bcf73c11507a23022
Aierken <salayidin103@gmail.com>
2024-02-12 03:35:12 -0500

    mongodb switch to development database

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,7 +5,7 @@ from models.visitor import DB_Visitor
 from models.game import DB_Games
 from config import *
 
-db_match = DB_Matchs(mongodb_link,"Matchs","game")
-db_user = DB_Users(mongodb_link,'Matchs','user')
-db_visitor = DB_Visitor(mongodb_link,'Matchs','visitor')
-# db_game = DB_Games(mongodb_link,"Matchs","game")
+db_match = DB_Matchs(mongodb_link,"Development","match")
+db_user = DB_Users(mongodb_link,'Development',"user")
+db_visitor = DB_Visitor(mongodb_link,'Development',"visitor")
+db_game = DB_Games(mongodb_link,"Development","game")
