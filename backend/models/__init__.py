865d98e32bd85d11285809f3a7b7920660054fa6
Aierken <salayidin103@gmail.com>
2024-02-20 23:24:27 -0500

    Env Variable name change

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,7 +5,7 @@ from models.visitor import DB_Visitor
 from models.game import DB_Games
 from config import *
 
-db_match = DB_Matchs(MONGODB_LINK,MONGODB_DATABASE_NAME,"match")
-db_user = DB_Users(MONGODB_LINK,MONGODB_DATABASE_NAME,"user")
-db_visitor = DB_Visitor(MONGODB_LINK,MONGODB_DATABASE_NAME,"visitor")
-db_game = DB_Games(MONGODB_LINK,MONGODB_DATABASE_NAME,"game")
+db_match = DB_Matchs(MONGODB_LINK,str(MONGODB_DATABASE_NAME),"match")
+db_user = DB_Users(MONGODB_LINK,str(MONGODB_DATABASE_NAME),"user")
+db_visitor = DB_Visitor(MONGODB_LINK,str(MONGODB_DATABASE_NAME),"visitor")
+db_game = DB_Games(MONGODB_LINK,str(MONGODB_DATABASE_NAME),"game")
